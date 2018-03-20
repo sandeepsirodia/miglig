dcc95eba962cab7fa63d5b2fea250be91843b3de
steve <stevehenry774@gmail.com>
2018-03-20 11:39:33 +0530

    static file changes

--- miglig/local_setting.py
+++ miglig/local_setting.py
@@ -80,7 +80,7 @@ AWS_LOCATION = 'static'
 #STATICFILES_DIRS = [
  #   os.path.join(BASE_DIR, 'static'),
 #]
-#STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
